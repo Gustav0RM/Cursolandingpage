b4274721f8b90cba6e4facae7a91e7510674a472
Gustavo R Martins <gustavo.roube.martins@hotmail.com>
2024-10-30 08:06:46 -0300

    Update

--- Comuns/header.php
+++ Comuns/header.php
@@ -3,12 +3,37 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Titulo exemplo</title>
 </head>
 <body>
-     
-
-
-
-</body>
-</html>
+  <header>
+    <div class="container">
+      <div class="conteudo">
+        <div class="logo">
+          <img src="../Imagens/logo.png" alt="logo">
+        </div>
+        <div class="menu-lateral">
+          <ul class="nav">
+            <li>
+              <a href="#">Quem sou eu</a>
+            </li>
+            <li>
+              <a href="#">Eventos</a>
+            </li>
+            <li>
+              <a href="#">Cursos</a>
+            </li>
+            <li>
+              <a href="#">Fale comigo</a>
+            </li>
+          </ul>
+          <div class="redes-sociais">
+            <p>Siga-me: </p>
+            <a href="#">Facebook</a>
+            <a href="#">Instagram</a>
+            <a href="#">Youtube</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
