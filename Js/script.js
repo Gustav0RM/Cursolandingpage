b40324b1b48bd677dd896ff81315175757f8ee3f
Gustavo R Martins <gustavo.roube.martins@hotmail.com>
2024-10-30 16:13:51 -0300

    update
    implementação do jquery para interações com o menu mobile

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function()
   {
-  alert("teste");
+  $('.nav-bar').on('click', function()
+    {
+    $('.menu-lateral').toggleClass('active'); // função para abrir ou fechar o menu no click
+    $('.nav-bar i').toggleClass("fa-bars fa-xmark"); // função para trocar o ícone de menu bars para um X, para fechar o menu
+    }
+  );
   }
 );
