d860afd90192327a44e67ffb15279e52623531c7
Gustavo R Martins <gustavo.roube.martins@hotmail.com>
2024-10-30 16:39:11 -0300

    update
    Criando a estrutura html conteúdo main

--- Views/home.php
+++ Views/home.php
@@ -1,3 +1,17 @@
 <main>
-  
+  <section class="quem-sou-eu">
+    <div class="container">
+      <div class="img">
+        <img src="../Imagens/pessoa-banner.png" alt="pessoa">
+      </div>
+      <div class="texto">
+        <h2>Quem sou eu</h2>
+        <div class="descriçao">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum distinctio perspiciatis nobis ea quam! Amet, sit hic fugit, est illo dignissimos ratione necessitatibus repudiandae iusto tempore, sunt dolor perspiciatis nostrum!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium obcaecati doloribus rerum labore nemo alias nulla facere recusandae, quam eos natus ipsa fugiat! Amet veritatis, optio nulla quasi numquam laboriosam.</p>
+        </div>
+        <a class="btn" href="#">Veja o vídeo</a>
+      </div>
+    </div>
+  </section>  
 </main>
