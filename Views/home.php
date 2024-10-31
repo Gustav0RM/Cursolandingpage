e88459673adbbe5f667056fed13088447c00dc2f
Gustavo R Martins <gustavo.roube.martins@hotmail.com>
2024-10-31 16:02:38 -0300

    update
    Estilização do main

--- Views/home.php
+++ Views/home.php
@@ -6,7 +6,7 @@
           <img src="../Imagens/pessoa-banner.png" alt="pessoa">
         </div>
         <div class="texto">
-          <h2>Quem sou eu</h2>
+          <h2>Quem sou eu?</h2>
           <div class="descriçao">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum distinctio perspiciatis nobis ea quam! Amet, sit hic fugit, est illo dignissimos ratione necessitatibus repudiandae iusto tempore, sunt dolor perspiciatis nostrum!</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium obcaecati doloribus rerum labore nemo alias nulla facere recusandae, quam eos natus ipsa fugiat! Amet veritatis, optio nulla quasi numquam laboriosam.</p>
